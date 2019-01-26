5dae362420a370fcfd132152b18dc05cf2e81f01
SrinivasMaka <srinivasmaka@gmail.com>
2019-01-26 22:35:58 +0530

    First Commit

---
+++ app/views/students/_student.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! student, :id, :name, :roll_no, :,phone, :,, :address, :created_at, :updated_at
+json.url student_url(student, format: :json)
---
+++ db/migrate/20190126165037_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :roll_no
+      t.string :phone
+      t.string :address
+
+      t.timestamps null: false
+    end
+  end
+end
